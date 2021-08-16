3f1969ba4915d71abb331eb68c79265cd535bd86
kephanyamweya <kephanyamweya95@gmail.com>
2021-08-16 12:18:12 +0300

    add images to the project

---
+++ src/main/java/models/Hero.java
@@ -0,0 +1,41 @@
+package models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Hero {
+    private String name;
+    private int age;
+    private String power;
+    private String weakness;
+    private int id;
+    private static ArrayList<Hero> instances = new ArrayList<>();
+
+
+
+    public Hero (String name, Integer age, String power,String weakness) {
+        this.name = name;
+        this.age = age;
+        this.power = power;
+        this.weakness = weakness;
+        instances.add(this);
+        this.id = instances.size();
+    }
+    public String getName() {
+        return this.name;
+    }
+    public int getAge() {return this.age;}
+    public String getPower() {return this.power;}
+    public String getWeakness() {return this.weakness;}
+    public static ArrayList<Hero> getAllInstances(){return instances;}
+    public static void clearAllHeroes() {instances.clear();}
+    public int getId() {return id;}
+    public static Hero findById(int id) {return instances.get(id-1);}
+
+
+    public static Hero setUpNewHero() {return new Hero("Timmie",26,"invisible","cute");}
+    public static Hero setUpNewHero1() {return new Hero("Dee",20,"speed","water");}
+    public static Hero setUpNewHero2() {return new Hero("Fixer",19,"strength","bee sting");}
+
+
+}
---
+++ src/test/java/models/HeroTest.java
@@ -0,0 +1,58 @@
+package models;
+
+import org.junit.Test;
+
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class HeroTest {
+    public void newHero_instantiatesCorrectly_true() {
+        Hero newHero = Hero.setUpNewHero();
+        assertTrue(newHero instanceof Hero);
+    }
+    @Test
+    public void newHero_getName_String() {
+        Hero newHero = Hero.setUpNewHero();
+        assertEquals("Timmie",newHero.getName());
+    }
+    @Test
+    public void newHero_getAge_Int() {
+        Hero newHero = Hero.setUpNewHero();
+        assertEquals(26,newHero.getAge());
+    }
+    @Test
+    public void newHero_getPower_String() {
+        Hero newHero = Hero.setUpNewHero();
+        assertEquals("invincible",newHero.getPower());
+    }
+    @Test
+    public void newHero_getWeakness_String() {
+        Hero newHero = Hero.setUpNewHero();
+        assertEquals("water",newHero.getWeakness());
+    }
+    @Test
+    public void newHero_getAllInstances_true() {
+        Hero newHero = Hero.setUpNewHero();
+        Hero another = Hero.setUpNewHero();
+        assertTrue(Hero.getAllInstances().contains(newHero));
+        assertTrue(Hero.getAllInstances().contains(another));
+    }
+    @Test
+    public void newHero_getId_Int() {
+        Hero.clearAllHeroes();
+        Hero newHero = Hero.setUpNewHero();
+        Hero another = Hero.setUpNewHero();
+        Hero another1 = Hero.setUpNewHero();
+        assertEquals(3,another1.getId());
+    }
+    @Test
+    public void newHero_findById_id() {
+        Hero.clearAllHeroes();
+        Hero newHero = Hero.setUpNewHero();
+        Hero another = Hero.setUpNewHero();
+        assertEquals(2,Hero.findById(another.getId()).getId());
+   }
+
+}
